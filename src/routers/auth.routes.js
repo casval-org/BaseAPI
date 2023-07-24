7c583cb62d3639574f8fbe2753ac4c56a928a83f
berk <berk-karatas@protonmail.com>
2023-07-25 00:27:29 +0300

    added user functions in auth.controller.js

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -6,6 +6,10 @@ const {
   forgetPassword,
   resetCodeCheck,
   resetPassword,
+  getAllUsers,
+  getUserbyId,
+  updateProfile,
+  deleteUser,
 } = require("../controllers/auth.controller");
 const authValidation = require("../middlewares/validations/auth.validation");
 const { tokenCheck } = require("../middlewares/auth");
@@ -21,5 +25,26 @@ router.post("/forget-password", forgetPassword);
 router.post("/reset-code-check", resetCodeCheck);
 
 router.post("/reset-password", resetPassword);
+
+router.patch("/update-profile/:id", updateProfile);
+
+router.get("/get-user/:id", getUserbyId);
+
+router.get("/get-users", getAllUsers);
+
+router.delete("/delete-user/:id", deleteUser);
+
+
+
+
+// router.get("/get-users", getUsers);
+
+
+
+// get-all-users
+// get-user/:id
+// delete-user/:id
+// update-profile/:id
+
 
 module.exports = router;
